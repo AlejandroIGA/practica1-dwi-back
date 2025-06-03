e268f19761473d02adb76b8bad537a7fbd159930
Alejandro Infante <alejandroinfante.ga@gmail.com>
2025-06-02 18:43:44 -0600

    Creación del proyecto

---
+++ src/main/java/com/edu/uteq/practica1_backend/Practiaca1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.edu.uteq.practica1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practiaca1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practiaca1BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/uteq/practica1_backend/Practiaca1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.uteq.practica1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practiaca1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
