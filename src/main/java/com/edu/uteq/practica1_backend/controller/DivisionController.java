6a965a4e8f5c0af1c3b44b2f95ad99b9972f872f
Jafet-a11 <145033871+Jafet-a11@users.noreply.github.com>
2025-06-03 11:37:31 -0600

    Se agregan los 2 metodos de buscary uno de crear

--- src/main/java/com/edu/uteq/practica1_backend/controller/DivisionController.java
+++ src/main/java/com/edu/uteq/practica1_backend/controller/DivisionController.java
@@ -1,9 +1,12 @@
 package com.edu.uteq.practica1_backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.edu.uteq.practica1_backend.model.entity.Division;
 import com.edu.uteq.practica1_backend.model.repository.DivisionRepo;
 
 @RestController
@@ -11,4 +14,29 @@ import com.edu.uteq.practica1_backend.model.repository.DivisionRepo;
 public class DivisionController {
     @Autowired
     private DivisionRepo repo;
+
+    @GetMapping()
+    // Busqueda para todos los datos
+    public List<Division> buscarTodos(@RequestParam boolean soloActivos) {
+        if (soloActivos) {
+            return repo.findAll().stream().filter(Division::isActivo).toList();
+        }
+        return repo.findAll();
+    }
+
+    // Buscar por id
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable int id) {
+        return repo.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Insertar
+    @PostMapping()
+    public ResponseEntity<?> crear(@RequestBody Division d) {
+        Division entity = repo.save(d);
+        return ResponseEntity.ok(entity);
+    }
+
 }
