6102f50fa1851d1a83a9cf3e3ce20a257c6fcb0f
Alejandro Infante <alejandroinfante.ga@gmail.com>
2025-06-03 13:16:22 -0600

    Se añaden métodos editar y eliminar par Division

--- src/main/java/com/edu/uteq/practica1_backend/controller/DivisionController.java
+++ src/main/java/com/edu/uteq/practica1_backend/controller/DivisionController.java
@@ -1,9 +1,18 @@
 package com.edu.uteq.practica1_backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edu.uteq.practica1_backend.model.entity.Division;
@@ -39,4 +48,27 @@ public class DivisionController {
         return ResponseEntity.ok(entity);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@PathVariable int id, @RequestBody Division d) {
+        Optional<Division> opt = repo.findById(id);
+        if (opt.isPresent()){
+            Division entity = opt.get();
+            entity.setClave(d.getClave());
+            entity.setNombre(d.getNombre());
+            entity.setActivo(d.isActivo());
+
+            return ResponseEntity.ok(repo.save(entity));
+        }        
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable int id){
+        Optional<Division> opt = repo.findById(id);
+        if(opt.isPresent()){
+            repo.deleteById(id);    
+            return ResponseEntity.noContent().build();    
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
